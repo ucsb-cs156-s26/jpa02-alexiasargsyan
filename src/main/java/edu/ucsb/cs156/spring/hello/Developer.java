f163cbc8ec34dcd60426f8462a6bdefbca726301
alexiasargsyan <alexiasargsyan@ucsb.edu>
2026-04-09 14:42:47 -0700

    as - updated with actual developer name and github id

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Alexia";
     }
 
     /**
@@ -29,8 +25,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "alexiasargsyan";
     }
 
     /**
@@ -39,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-05");
+        team.addMember("Alexia");
+        team.addMember("Elissa");
+        team.addMember("Ibrahim");
+        team.addMember("Joaquin D.");
+        team.addMember("John K.");
+        team.addMember("Soome");
         return team;
     }
 }
